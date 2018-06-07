b74f3c206fcc44d8e88ecd34c181fd4d0bc76a62
Excedium <38740912+Excedium@users.noreply.github.com>
2018-06-06 23:20:39 -0400

    overhaul

--- assignments/lesson-6/js/currentdate.js
+++ assignments/lesson-6/js/currentdate.js
@@ -1,3 +1,30 @@
-function showDate(element) {
-  element.text = new Date().toString();
+ function dayName() {
+    var d = new Date(1);
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var n = weekday[d.getDay()];
+    document.getElementById("dayname").innerHTML = n;
+}
+
+function fullYear() {
+    var d = new Date();
+    document.getElementById("year").innerHTML = d.getFullYear();
+}
+
+function monthName() {
+	var d = new Date();
+       var months = ["January", "February", "March", "April", "May", "June", "July", "August", “September", "October", "November", "December"];
+       document.getElementById("monthname").innerHTML = months[d.getMonth()];
+}
+
+function numberDay() {
+	var d = new Date();
+	document.getElementById("numberday").innerHTML = d.getDate();
 }
