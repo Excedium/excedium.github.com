a794fdffe58ca62b7566c1dee95733bf02f518a1
Excedium <38740912+Excedium@users.noreply.github.com>
2018-06-09 12:13:50 -0400

    many many changes

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -1,16 +1,9 @@
 function doInputOutput()
 {
-    var h = get("high").value;
-    var l = get("low").value;    
-    var s = get("speed").value;
-    var temp = (h+l)/2
-    var f = windChill(temp, speed);
-    document.getElementById("output").innerHTML = Math.round(f * 100)/100 + " Fahrenheit";
+    var h = parseFloat(document.getElementById("high").innerHTML);
+    var l = parseFloat(document.getElementById("low").innerHTML);    
+    var s = parseFloat(document.getElementById("speed").innerHTML);
+    var temp = (h + l) / 2;
+    var f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(s, 0.16) + 0.4275 * temp * Math.pow(s, 0.16);
+    document.getElementById("output").innerHTML = Math.round(f * 1)/1;
 }
-
-function windChill(t, s)
-{
-var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-return f;
-}
-
