914266910b5991ae3fc50ec99ed6d00b5d105778
Excedium <38740912+Excedium@users.noreply.github.com>
2018-06-30 15:09:19 -0400

    h

--- assignments/weathersite/js/include.js
+++ assignments/weathersite/js/include.js
@@ -1,27 +1,25 @@
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-   
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      } 
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      
-      return;
-    }
-  }
+function include() {
+let file, xhttp;
+
+let allElements = document.getElementsByTagName('*'); // get all elements on the page
+
+for (let i = 0; i < allElements.length; i++) { // process all the elements looking for the custom attribute 'includefile'
+file = allElements[i].getAttribute("includefile");
+if (file) { // if the attribute is found, process an AJAX XMLHttpRequest
+xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+if (this.readyState == 4) {
+if (this.status == 200 || this.status == 0) { allElements[i].innerHTML = this.responseText;}
+if (this.status == 404) { allElements[i].innerHTML = "Page not found!";}
+allElements[i].removeAttribute('includefile');
+include(); // call the function again to start the process over
 }
+}
+xhttp.open('GET', 'modules/' + file, true); // matching file names in includes folder
+xhttp.send();
+return;
+}
+}
+}
+// initial call to the include function
+include();
